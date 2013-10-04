6dc6420b9fa8264111d30ef9f6a6a589cf1d6bf6
pm <mitin.pavel@gmail.com>
2013-10-04 14:01:52 +0300

    Disable ipv6.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,7 @@
 hduser = 'hduser'
 
-# Create a user and a group
+# Create a user and a group ---------------------------------------------------
+
 group "hadoop"
 
 user hduser do
@@ -10,11 +11,30 @@ user hduser do
   shell "/bin/bash"
 end
 
-# Setup SSH
+# Setup SSH -------------------------------------------------------------------
+
 execute "generate ssh keys for #{hduser}." do
   user hduser
   command <<-SHELL
     ssh-keygen -t rsa -q -f /home/#{hduser}/.ssh/id_rsa -P ""
     cat /home/#{hduser}/.ssh/id_rsa.pub >> /home/#{hduser}/.ssh/authorized_keys
   SHELL
+  not_if { File.exist?("/home/#{hduser}/.ssh/id_rsa.pub") }
+end
+
+# Disable ipv6 ----------------------------------------------------------------
+
+config_file = "/etc/sysctl.conf"
+
+lines = <<-COMMANDS
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.lo.disable_ipv6 = 1
+COMMANDS
+
+lines.each_line do |line|
+  execute "add #{line} to #{config_file}" do
+    command "echo '#{line}' >> #{config_file}"
+    only_if { File.readlines(config_file).detect { |l| l.include?(line)}.nil? }
+  end
 end

36f7097623aff03f6bed7fcbc641e881e77395cf
pm <mitin.pavel@gmail.com>
2013-10-04 11:44:14 +0300

    Create an empty cookbook.

---
+++ metadata.rb
@@ -0,0 +1,11 @@
+name "single_node_hadoop_claster"
+
+description      "Installs and configures Hadoop 1.0.x on a Single Linux Node"
+version          "0.0.1"
+
+maintainer       "Pavel Mitin"
+maintainer_email "mitin.pavel@gmail.com"
+
+license          "MIT"
+
+depends          "java"
